14ad1f7e8395ef0da04778390df07fcf7b68d433
Yoqub <yyboboraximov@gmail.com>
2022-07-22 07:12:45 +0500

    task3_attempts3

--- src/main/java/lang/print/gaps/task2/AdvancedNamingConvention.java
+++ src/main/java/lang/print/gaps/task2/AdvancedNamingConvention.java
@@ -1,7 +1,7 @@
 package lang.print.gaps.task2;
 
-public class advancedNamingConvention {
-    private static final int adult_age = 18;
+public class AdvancedNamingConvention {
+    private static final int ADULT_AGE = 18;
 
     private int age;
     private int phoneNumber;
