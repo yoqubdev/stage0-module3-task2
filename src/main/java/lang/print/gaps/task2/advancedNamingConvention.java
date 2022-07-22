74ffa92f705cfa0c57a9f7f87de36a823561fbe8
Yoqub <yyboboraximov@gmail.com>
2022-07-22 06:45:31 +0500

    task3

--- src/main/java/lang/print/gaps/task2/advancedNamingConvention.java
+++ src/main/java/lang/print/gaps/task2/advancedNamingConvention.java
@@ -1,16 +1,16 @@
 package lang.print.gaps.task2;
 
 public class advancedNamingConvention {
-    private static final int adult_age = 18;
+    private static final int ADULT_AGE = 18;
 
-    private int AGE;
+    private int age;
     private int phoneNumber;
 
-    void CAllToFriend() {
-        //some code
+    void callToFriend() {
+        System.out.println("hey friend!");
     }
 
-    void callByNumber(int Number) {
-        //some code
+    void callByNumber(int number) {
+        System.out.println("seven");
     }
 }
